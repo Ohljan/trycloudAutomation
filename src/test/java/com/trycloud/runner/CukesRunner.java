200b9d9dbfb95f2b39e971fa16ce695eff65ea2f
ohljanzunun <zununohljan@gmail.com>
2022-03-25 12:44:52 -0400

    all set

---
+++ src/test/java/com/trycloud/runner/CukesRunner.java
@@ -0,0 +1,27 @@
+package com.trycloud.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/trycloud/step_definitions",
+        features ="src/test/resources/features",
+        plugin =  {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+
+        },
+        dryRun = false,
+        tags = "",
+        publish=true
+
+
+
+)
+public class CukesRunner {
+
+}
