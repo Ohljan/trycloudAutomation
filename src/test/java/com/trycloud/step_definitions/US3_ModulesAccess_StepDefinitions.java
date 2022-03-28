8bc1853f803e465cc8b91c6edd64c9ebfff91972
Asyl Orozmatova <asylumaraly@gmail.com>
2022-03-28 17:11:49 -0400

    US3 StepDefinitions class created with snippets

---
+++ src/test/java/com/trycloud/step_definitions/US3_ModulesAccess_StepDefinitions.java
@@ -0,0 +1,29 @@
+package com.trycloud.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US3_ModulesAccess_StepDefinitions {
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the users log in with the valid credentials")
+    public void the_users_log_in_with_the_valid_credentials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("verify the users see the following modules")
+    public void verify_the_users_see_the_following_modules(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+}
--- src/test/resources/features/us3_modulesAccess.feature
+++ src/test/resources/features/us3_modulesAccess.feature
@@ -1,5 +1,5 @@
 Feature: As a user, I should be accessing all the main modules of the app
-
+@us3
   Scenario: Verify users accessing all the main modules of the app
     Given user is on the login page
     When the users log in with the valid credentials
