027a596f2528ef0727be5e363630dc1747e53de0
enes <enesuzun2020@gmail.com>
2022-03-29 11:56:01 -0400

    us9 is done

--- src/test/java/com/trycloud/step_definitions/US9_stepdefinitions.java
+++ src/test/java/com/trycloud/step_definitions/US9_stepdefinitions.java
@@ -2,6 +2,7 @@ package com.trycloud.step_definitions;
 
 import com.trycloud.pages.FilesPage;
 import com.trycloud.pages.LoginPage;
+import com.trycloud.utilities.BrowserUtils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -10,37 +11,39 @@ import org.junit.Assert;
 public class US9_stepdefinitions {
     FilesPage filesPage=new FilesPage();
 
-    @Given("user on the dashboard pageS")
-    public void user_on_the_dashboard_pageS() {
-        LoginPage loginPage = new LoginPage();
-        loginPage.login();
-    }
-    @When("the user clicks the {string} moduleS")
-    public void the_user_clicks_the_moduleS(String string) {
-        filesPage.files.click();
-    }
-    @When("user click action-icon  from any file on the pageS")
-    public void user_click_action_icon_from_any_file_on_the_pageS() {
+
+
+
+
+
+
+    @When("user click action-icon  from any file on the page")
+    public void user_click_action_icon_from_any_file_on_the_page() {
         filesPage.actionIcon.click();
     }
-    @When("user choose the {string} optionS")
-    public void user_choose_the_optionS(String string) {
+    @When("user choose the {string} option")
+    public void user_choose_the_option(String string) {
         filesPage.details.click();
     }
-    @When("user write a comment inside the input boxS")
-    public void user_write_a_comment_inside_the_input_boxS() {
-    filesPage.comments.click();
-    filesPage.message.sendKeys("good job");
+    @When("user write a {string} inside the input box")
+    public void user_write_a_inside_the_input_box(String myMessage) {
+        filesPage.comments.click();
+        filesPage.setMessage(myMessage);
+        BrowserUtils.sleep(3);
     }
-    @When("user click the submit button to post itS")
-    public void user_click_the_submit_button_to_post_itS() {
-    filesPage.post.click();
-    }
-    @Then("Verify the comment is displayed in the comment sectionS.")
-    public void verify_the_comment_is_displayed_in_the_comment_sectionS() {
-        String actualText = filesPage.seePost.getText();
-        String expectedText="good job";
-        Assert.assertEquals(expectedText,actualText);
 
+    @When("user click the submit button to post it")
+    public void user_click_the_submit_button_to_post_it() {
+        filesPage.post.click();
+    }
+
+    @Then("Verify the {string} is displayed in the comment section.")
+    public void verify_the_is_displayed_in_the_comment_section(String expectedText) {
+        BrowserUtils.sleep(3);
+        String actualText = filesPage.seePost.getText();
+        BrowserUtils.sleep(2);
+        Assert.assertEquals(expectedText,actualText);
+        System.out.println("actualText = " + actualText);
+        System.out.println("expectedText = " + expectedText);
     }
 }
