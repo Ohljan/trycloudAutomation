622a39589243dbe36bd72a84c0298243f7547248
Asyl Orozmatova <asylumaraly@gmail.com>
2022-03-29 07:30:40 -0400

    US3 StepDefinition finished and ready for review to merge

--- src/test/java/com/trycloud/step_definitions/US3_ModulesAccess_StepDefinitions.java
+++ src/test/java/com/trycloud/step_definitions/US3_ModulesAccess_StepDefinitions.java
@@ -1,29 +1,49 @@
 package com.trycloud.step_definitions;
 
+import com.trycloud.pages.LoginPage;
+import com.trycloud.pages.ModulesPage;
+import com.trycloud.utilities.BrowserUtils;
+import com.trycloud.utilities.ConfigurationReader;
+import com.trycloud.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class US3_ModulesAccess_StepDefinitions {
+
+    LoginPage loginPage = new LoginPage();
+    ModulesPage modulesPage = new ModulesPage();
+
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        //  loginPage.login(); -> allows me to combine 2 steps
+        Driver.getDriver().get(ConfigurationReader.getProperty("env2"));
     }
+
     @When("the users log in with the valid credentials")
     public void the_users_log_in_with_the_valid_credentials() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        loginPage.userNameInput.sendKeys(ConfigurationReader.getProperty("username1"));
+        loginPage.passwordInput.sendKeys(ConfigurationReader.getProperty("password"));
+        loginPage.submitButton.click();
+
     }
+
     @Then("verify the users see the following modules")
-    public void verify_the_users_see_the_following_modules(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+    public void verify_the_users_see_the_following_modules(List<String> expectedModules) {
+
+        List<String> actualModules = new ArrayList<>();
+
+        for (WebElement each : modulesPage.mainModules) {
+            BrowserUtils.hover(each);
+            actualModules.add(each.getText());
+        }
+
+        Assert.assertEquals(expectedModules, actualModules);
     }
-}
+    }
+
